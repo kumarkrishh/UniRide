f2f8c8e612594fa7851d1798a178983cc538419f
Krish <krishkumar@Krishs-MacBook-Pro.local>
2024-06-15 22:13:08 -0700

    added some more text to main page

--- app/page.jsx
+++ app/page.jsx
@@ -2,14 +2,11 @@ import Feed from "@components/Feed";
 
 const Home = () => (
   <section className='w-full flex-center flex-col'>
-    <h1 className='head_text text-center'>
-      Discover & Share
-      <br className='max-md:hidden' />
-      <span className='orange_gradient text-center'> AI-Powered Prompts</span>
+    <h1 className='head_text text-center blue_gradient'>
+      Connect & Commute
     </h1>
     <p className='desc text-center'>
-      Promptopia is an open-source AI prompting tool for modern world to
-      discover, create and share creative prompts
+    BruinRide is a platform for college students to connect, carpool, and cut costs. Enhance your college experience by finding travel buddies and sharing rides sustainably within your community.
     </p>
 
     <Feed />
