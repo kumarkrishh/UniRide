325212607a177fd5bfd8f3249cb23b1daae64b1b
Krish <krishkumar@Krishs-MacBook-Pro.local>
2024-06-15 22:06:03 -0700

    added logo on top right

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -22,13 +22,13 @@ const Nav = () => {
     <nav className='flex-between w-full mb-16 pt-3'>
       <Link href='/' className='flex gap-2 flex-center'>
         <Image
-          src='/assets/images/logo.svg'
+          src='/assets/images/logo4.png'
           alt='logo'
-          width={30}
+          width={125}
           height={30}
           className='object-contain'
         />
-        <p className='logo_text'>Promptopia</p>
+        
       </Link>
 
       {/* Desktop Navigation */}
