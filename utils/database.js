5d85ca2b9e76eb018fb649a46939972fec25327c
Krish <krishs.kumar14@gmail.com>
2024-08-12 01:13:47 -0700

    testing db

--- utils/database.js
+++ utils/database.js
@@ -1,25 +1,27 @@
 import mongoose from 'mongoose';
 
+let isConnected = false; // track the connection
+
 export const connectToDB = async () => {
   mongoose.set('strictQuery', true);
 
-  if (mongoose.connection.readyState === 1) {
+  if(isConnected) {
     console.log('MongoDB is already connected');
     return;
   }
 
-  const options = {
-    dbName: "share_prompt",
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    keepAlive: true, // Ensures that the connection is kept active
-    keepAliveInitialDelay: 300000 // Start sending keepAlive packets after 300000ms of inactivity
-  };
-
   try {
-    await mongoose.connect(process.env.MONGODB_URI, options);
-    console.log('MongoDB connected');
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "share_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      
+    })
+
+    isConnected = true;
+
+    console.log('MongoDB connected')
   } catch (error) {
-    console.error('Failed to connect to MongoDB:', error);
+    console.log(error);
   }
-};
+}
